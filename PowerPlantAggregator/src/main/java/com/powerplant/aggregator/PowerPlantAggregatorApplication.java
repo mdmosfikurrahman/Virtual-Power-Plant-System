9d17e236e855734ec2849c0bf705ecf69f7c8ae3
Mosfik <rahman.mosfikur@revesoft.com>
2024-07-14 16:09:38 +0600

    Implemented Full Project Given in Brac IT Interview - Coding Test

---
+++ PowerPlantAggregator/src/main/java/com/powerplant/aggregator/PowerPlantAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.powerplant.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerPlantAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerPlantAggregatorApplication.class, args);
+    }
+
+}
---
+++ PowerPlantAggregator/src/test/java/com/powerplant/aggregator/PowerPlantAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerplant.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerPlantAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
